d7def4a95b7a3d2ba86d61b44952be45ea0c37fd
uskelkeron <k-cyrus@hotmail.com>
2022-02-22 00:29:37 -0400

    Alien spacecraft working properly, added canon

--- js/app.js
+++ js/app.js
@@ -1,26 +1,39 @@
-let FLY_DURATION = 0;
+let stopflights = 0;
 
-const flyspaceCraft = (spaceship,flightSpeed)=>
+const flyspaceCraft = (spaceship,flightSpeed,shipid)=>
 {
+    let FLY_DURATION=0 
 
     const flySpaceCraft1 = setInterval(() => 
     {
-      
-        console.log(flightSpeed+"flight speed");
-
-        FLY_DURATION = FLY_DURATION + flightSpeed;
-
+        
        
+
+        console.log(`Current Flight Speed :${flightSpeed}, Current Duration: ${FLY_DURATION}, Current Ship id: ${shipid}, Stopflight value ${stopflights}`);
         
-        spaceship.style.marginTop = `${FLY_DURATION}%`;
+         
+
+            if(stopflights >= 60)
+            {
+                clearInterval(flySpaceCraft1);
+              
+            }
+            else
+            {
+                
+                FLY_DURATION += flightSpeed;
+
+                if(stopflights < FLY_DURATION)
+                {
+                    stopflights = FLY_DURATION;
+                }
+            }
+        
+        const spaceshipduration=spaceship.style.marginTop = `${FLY_DURATION}vh`;
     
-        if(FLY_DURATION>=120)
-        {
-            clearInterval(flySpaceCraft1);
-          
-        }
-        console.log(FLY_DURATION+"flight duration");
+   
         
+     
     }, 1000);
     
 
@@ -30,12 +43,7 @@ const main = (()=>
 {
 //Declaration of Variables
 const spaceCraft = document.querySelectorAll(".spaceCraft");
-
-
-console.log(spaceCraft);
-
-
-
+const canon = document.querySelector("#canon_container > div");
 let spaceCraft1 = spaceCraft[0];
 let spaceCraft2 = spaceCraft[1];
 let spaceCraft3 = spaceCraft[2];
@@ -44,23 +52,44 @@ let spaceCraft5 = spaceCraft[4];
 
 
 
-const flightSpeed = [15,15,15,15,15,15];
+const flightSpeed = [1,2,3,4,5,6];
 
-let random = Math.floor(Math.random()*5);
-flyspaceCraft(spaceCraft1,flightSpeed[random]);
+    let random = Math.floor(Math.random()*5);
+    flyspaceCraft(spaceCraft1,flightSpeed[random],"101");
 
-random = Math.floor(Math.random()*5);
-flyspaceCraft(spaceCraft2,flightSpeed[random]);
+    random = Math.floor(Math.random()*5);
+    flyspaceCraft(spaceCraft2,flightSpeed[random],"102");
 
-random = Math.floor(Math.random()*5);
-flyspaceCraft(spaceCraft3,flightSpeed[random]);
+    random = Math.floor(Math.random()*5);
+    flyspaceCraft(spaceCraft3,flightSpeed[random],"103");
 
-random = Math.floor(Math.random()*5);
-flyspaceCraft(spaceCraft4,flightSpeed[random]);
+    random = Math.floor(Math.random()*5);
+    flyspaceCraft(spaceCraft4,flightSpeed[random],"104");
 
-random = Math.floor(Math.random()*5);
-flyspaceCraft(spaceCraft5,flightSpeed[random]);
+    random = Math.floor(Math.random()*5);
+    flyspaceCraft(spaceCraft5,flightSpeed[random],"105");
 
+    let margin = 0;
+    document.addEventListener("keydown",(event)=>{
+        
+       
+
+        if(event.key == "ArrowLeft")
+        {
+            margin = margin - 10;
+           
+            canon.style.marginLeft = `${margin}px`
+           
+        }
+        if (event.key ==="ArrowRight")
+        {
+            margin = margin + 10;
+           
+            canon.style.marginLeft = `${margin}px`
+        }
+        
+        
+    })
 
 
 
